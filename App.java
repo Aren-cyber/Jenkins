352aac714dff178b2efb497f08af13eb7bfff89e
Aren1 <Aren.harutyunyan@gmail.com>
2021-11-12 05:28:29 -0800

    Adding It's me

--- App.java
+++ App.java
@@ -2,12 +2,12 @@ package com.bdg.training.jenkins;
 
 /**
  * Hello world!
- *
+ *It's me
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! It's me" );
     }
 }
