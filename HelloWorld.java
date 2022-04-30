9017605d740d10822e4ec3b67e1de153a730b70c
Aren-cyber <79959568+Aren-cyber@users.noreply.github.com>
2022-04-30 06:23:22 +0400

    Update and rename App.java to HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bdg.training.jenkins;
+
+/**
+ * Hello world!
+ *It's me
+ */
+public class HelloWorld{
+    public static void main( String args []){
+        System.out.println("Hello World! It's me");
+        }
+}
