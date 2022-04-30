ef4bf8f5123758c7243f0b7b32bbb92100445fa1
Aren-cyber <79959568+Aren-cyber@users.noreply.github.com>
2022-04-30 06:25:12 +0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,3 @@
-package com.bdg.training.jenkins;
-
-/**
- * Hello world!
- *It's me
- */
 public class HelloWorld{
     public static void main( String args []){
         System.out.println("Hello World! It's me");
